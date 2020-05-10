ca1b9c1137fc14ec29e89450501da15ea991ba7a
matthewcobbing <matthewc@nandos.co.uk>
2020-05-10 15:58:34 +0100

    run db migrate

--- api/src/state.rs
+++ api/src/state.rs
@@ -1,5 +1,7 @@
 use std::env;
 use sqlx::PgPool;
+use postgres::{Client, NoTls};
+use async_std::task;
 
 #[derive(Debug)]
 pub(crate) struct State {
@@ -11,6 +13,12 @@ impl State {
     pub(crate) async fn new() -> tide::Result<Self> {
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
         let db_pool = PgPool::new(&database_url).await?;
+
+        let mut client = Client::connect(&database_url, NoTls)?;
+
+        task::block_on(async {
+            database::migration::migrations::runner().run(&mut client).unwrap();
+        });
         
         Ok(Self { db: db_pool })
     }
