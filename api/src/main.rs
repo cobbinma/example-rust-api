129ebd87db203ec5c063c2d0ddea6666a0fec0e2
matthewcobbing <matthewc@nandos.co.uk>
2020-05-12 19:50:31 +0100

    add handlers and logger

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,17 +2,21 @@ use async_std::task;
 use dotenv::dotenv;
 
 mod state;
+mod handlers;
 
 use state::State;
 
 fn main() -> tide::Result<()> {
     task::block_on(async {
         dotenv().ok();
+        env_logger::init();
 
         let state = State::new().await?;
         let mut app = tide::with_state(state);
 
-        app.at("/").get(|_| async { Ok("Hello, world!") });
+        app.at("/pet").get(handlers::get_pets);
+        app.at("/pet").post(handlers::create_pet);
+        app.at("/pet/:id").get(handlers::get_pet);
 
         app.listen("127.0.0.1:8181").await?;
         Ok(())
