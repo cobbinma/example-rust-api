82a1da1132c26dcd76b5a0b1349dd147f5a8d093
matthewcobbing <matthewc@nandos.co.uk>
2020-05-12 23:35:50 +0100

    get db from pub fn

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -5,20 +5,20 @@ use crate::state::State;
 
 pub(crate) async fn get_pet(req: Request<State>) -> tide::Result<impl Into<Response>> {
     let id: i32 = req.param("id")?;
-    let pet = req.state().db.get_pet(id).await?;
+    let pet = req.state().db().get_pet(id).await?;
 
     Ok(Response::new(StatusCode::Ok).body_json(&pet)?)
 }
 
 pub(crate) async fn get_pets(req: Request<State>) -> tide::Result<impl Into<Response>> {
-    let pets = req.state().db.find_all().await?;
+    let pets = req.state().db().find_all().await?;
 
     Ok(Response::new(StatusCode::Ok).body_json(&pets)?)
 }
 
 pub(crate) async fn create_pet(mut req: Request<State>) -> tide::Result<impl Into<Response>> {
     let pet: Pet = req.body_json().await?;
-    req.state().db.create_pet(&pet).await?;
+    req.state().db().create_pet(&pet).await?;
 
     Ok(Response::new(StatusCode::Ok))
 }
