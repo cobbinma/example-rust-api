f500c82b17b860cb4f615f68873534c513d79176
matthewcobbing <matthewc@nandos.co.uk>
2020-05-12 22:25:37 +0100

    health check

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,17 @@
 use async_std::task;
 use dotenv::dotenv;
-use tide::log;
+use tide::{log, Response, StatusCode};
 
-mod state;
 mod handlers;
+mod state;
 
 use state::State;
 
 fn main() -> tide::Result<()> {
     task::block_on(async {
-        femme::ndjson::Logger::new().start(log::Level::Info.to_level_filter()).unwrap();
+        femme::ndjson::Logger::new()
+            .start(log::Level::Info.to_level_filter())
+            .unwrap();
         dotenv().ok();
 
         let state = State::new().await?;
@@ -18,6 +20,9 @@ fn main() -> tide::Result<()> {
         app.at("/pet").get(handlers::get_pets);
         app.at("/pet").post(handlers::create_pet);
         app.at("/pet/:id").get(handlers::get_pet);
+
+        app.at("/healthz")
+            .get(|_| async { Ok(Response::new(StatusCode::Ok)) });
 
         app.listen("127.0.0.1:8181").await?;
         Ok(())
