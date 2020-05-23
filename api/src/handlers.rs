f0b6c4766861cabceac24ae9f4f3497c51f7f54f
matthewcobbing <matthewc@nandos.co.uk>
2020-05-23 12:44:45 +0100

    return errors from get pets handler

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -27,9 +27,16 @@ pub(crate) async fn get_pet(req: Request<State>) -> tide::Result<impl Into<Respo
 }
 
 pub(crate) async fn get_pets(req: Request<State>) -> tide::Result<impl Into<Response>> {
-    let pets = req.state().db().find_all().await?;
-
-    Ok(Response::new(StatusCode::Ok).body_json(&pets)?)
+    match req.state().db().find_all().await {
+        Ok(pets) => Ok(Response::new(StatusCode::Ok).body_json(&pets)?),
+        Err(e) => {
+            warn!("Error getting pets from database: {:?}", e);
+            Ok(
+                Response::new(StatusCode::InternalServerError)
+                    .body_json(&ErrorResponse::from(e))?,
+            )
+        }
+    }
 }
 
 pub(crate) async fn create_pet(mut req: Request<State>) -> tide::Result<impl Into<Response>> {
