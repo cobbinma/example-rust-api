d35d1ee0172eee58e4077807eb1c31fe81310316
cobbinma <matthewc@nandos.co.uk>
2020-06-25 08:28:32 +0100

    add repository trait

---
+++ models/src/repository.rs
@@ -0,0 +1,13 @@
+use async_trait::async_trait;
+use std::error::Error;
+
+use crate::pet::Pet;
+
+#[async_trait]
+pub trait Repository {
+    async fn new() -> Self;
+
+    async fn get_pet(&self, id: i32) -> Result<Pet, Box<dyn Error>>;
+    async fn create_pet(&self, pet: &Pet) -> Result<(), Box<dyn Error>>;
+    async fn find_all(&self) -> Result<Vec<Pet>, Box<dyn Error>>;
+}
